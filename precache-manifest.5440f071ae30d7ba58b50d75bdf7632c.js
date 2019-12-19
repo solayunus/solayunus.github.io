95fb70eadf3e9b8eace8905eb20e8671715666cb
solayunus <solayunus19@gmail.com>
2019-12-19 09:27:07 +0100

    react update

---
+++ precache-manifest.5440f071ae30d7ba58b50d75bdf7632c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d26fce604820216ad8b837d066f7be",
+    "url": "/gitpage1/index.html"
+  },
+  {
+    "revision": "f8424a40dff05e3bdf85",
+    "url": "/gitpage1/static/css/main.507336ad.chunk.css"
+  },
+  {
+    "revision": "43b0a11ec46d28b99296",
+    "url": "/gitpage1/static/js/2.925c495e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gitpage1/static/js/2.925c495e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8424a40dff05e3bdf85",
+    "url": "/gitpage1/static/js/main.1adc3e74.chunk.js"
+  },
+  {
+    "revision": "66f053835e78096eba08",
+    "url": "/gitpage1/static/js/runtime-main.eebe08fc.js"
+  }
+]);
